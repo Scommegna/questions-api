0dfb4d051deae5e724173393ad71e5db8233d37c
Scommegna <scommegnal@hotmail.com>
2026-01-31 15:12:08 -0300

    feat: created user repo, service and controller

---
+++ src/main/java/com/freequestions/questions_api/repositories/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.freequestions.questions_api.repositories.user;
+
+import com.freequestions.questions_api.models.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
