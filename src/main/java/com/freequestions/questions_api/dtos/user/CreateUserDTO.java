479d615863f36f855e2ee35f03d7a2989384468b
Scommegna <scommegnal@hotmail.com>
2026-01-31 18:12:45 -0300

    fix: adjusted datetime provider of JpaAuditing

--- src/main/java/com/freequestions/questions_api/dtos/user/CreateUserDTO.java
+++ src/main/java/com/freequestions/questions_api/dtos/user/CreateUserDTO.java
@@ -13,7 +13,7 @@ public class CreateUserDTO {
     @NotBlank(message = "Password is mandatory")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
             message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
     )
     private String password;
