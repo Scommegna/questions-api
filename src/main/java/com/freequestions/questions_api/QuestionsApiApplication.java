d5c9c804dbe76c89a09536bdb27ff64a82348ed5
Scommegna <scommegnal@hotmail.com>
2026-01-31 12:14:39 -0300

    first commit

---
+++ src/main/java/com/freequestions/questions_api/QuestionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.freequestions.questions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freequestions/questions_api/QuestionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freequestions.questions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
